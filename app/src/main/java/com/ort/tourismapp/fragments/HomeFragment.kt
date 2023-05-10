85353c3ce9c06ac57337b1d6823643ac571d5afb
iasiris <baenamariana@gmail.com>
2023-05-10 16:15:14 -0300

    se agrego firebase para registration, falta retrive de datos al momento de loggearse

--- app/src/main/java/com/ort/tourismapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/ort/tourismapp/fragments/HomeFragment.kt
@@ -1,16 +1,24 @@
 package com.ort.tourismapp.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 import com.ort.tourismapp.R
+import com.ort.tourismapp.entities.User
 
 class HomeFragment : Fragment() {
 
@@ -21,7 +29,13 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
     lateinit var v : View
 
+    private lateinit var firebaseAuth : FirebaseAuth
     lateinit var txtSearch : EditText
+    lateinit var txtNombre : TextView
+    lateinit var currentUserId : String
+
+    val user = Firebase.auth.currentUser
+    val userId = user?.uid
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,11 +43,13 @@ class HomeFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.fragment_home, container, false)
         txtSearch = v.findViewById(R.id.txtSearch)
+        txtNombre = v.findViewById(R.id.homeUsername)
         return v
     }
 
     override fun onStart() {
         super.onStart()
+        getUserData()
         txtSearch.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 val searchTerm = txtSearch.text.toString()
@@ -52,4 +68,23 @@ class HomeFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    fun getUserData(){
+        val database = FirebaseDatabase.getInstance().getReference("Users")
+
+        if (userId != null) {
+            database.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    val user = snapshot.getValue(User::class.java)
+                    txtNombre.text= user?.name.toString()
+                }
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+        }
+    }
 }
+
+
+
+
